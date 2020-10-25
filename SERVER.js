a4d79d28e35092e294c229c492aa7a14d2f3a4c9
rebelspirit <vv.malinovskyi@gmail.com>
2020-10-26 00:44:56 +0200

    v1.0.1

--- SERVER.js
+++ SERVER.js
@@ -4,7 +4,6 @@ const http = require('https');
 const fs = require('fs');
 const Path = require('path');
 const cron = require("node-cron");
-const webp = require('webp-converter');
 const {ungzip} = require('node-gzip');
 const colors = require('colors');
 const mongoose = require('mongoose');
@@ -12,6 +11,8 @@ const movie = require('./models/movie.js');
 const allMoviesID = require('./models/allMoviesID.js');
 const partialMovie = require('./models/partial_movie.js');
 const readlineSync = require('readline-sync');
+const imageMin = require('imagemin');
+const imageminWebp = require('imagemin-webp');
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -28,9 +29,9 @@ const region = 'region=RU';
 // });
 
 const countOfParsingItems = 20;
-const delay = 10000;
+const delay = 20000;
 
-const deleteItem = (item) => {
+const deleteItem = item => {
     fs.stat(item, (err) => {
         if (err) return console.error(err);
 
@@ -40,25 +41,33 @@ const deleteItem = (item) => {
         });
     });
 };
-const convertImages = (input, output) => {
-    webp.cwebp(input, output,"-q 80",(status, error) => {
-        // if conversion successful status will be '100'
-        // if conversion fails status will be '101'
-        if (status === "100") {
-            console.log(`[CONVERTER]: Image convert successfully! And new link is:`.green, colors.cyan(output))
-        } else {
-            console.log(status, error);
-        }
-    })
+
+const deleteFolder = folder => {
+    fs.rmdirSync(folder, { recursive: true });
+    console.log(`[SERVER]: Folder with JPG images deleted: ${colors.cyan(folder)}`.green);
 };
 
-const createFolder = (name) => {
+const convertImages = async (input, output) => {
+    try {
+        await imageMin([input], {
+            destination: output,
+            plugins: [
+                imageminWebp({quality: 75})
+            ]
+        });
+        console.log(`[SERVER]: All images from folder ${colors.cyan(input)}`.green, `successfully converted and now destination is ${colors.cyan(output)}`.green);
+    } catch (e) {
+        console.log(colors.red(e.message))
+    }
+};
+
+const createFolder = name => {
     fs.mkdir(Path.join(__dirname, name),
-        { recursive: false }, (err) => {
+        { recursive: false }, err => {
             if (err) {
-                return console.log(`[INITIALIZATION]: Directory already exists!`.yellow, colors.cyan(name));
+                return console.log(`[SERVER]: Directory already exists!`.green, colors.cyan(name));
             }
-            console.log(`[INITIALIZATION]: Directory created successfully!`.yellow, colors.cyan(name));
+            console.log(`[SERVER]: Directory created successfully!`.green, colors.cyan(name));
         });
 };
 
@@ -101,10 +110,11 @@ const downloadFile = (async (url, path) => {
     });
 });
 
-const getFullDayBackup = async (date) => {
+const getFullDayBackup = async date => {
     try {
         const name = `${date}.json.gz`;
-        const url = `http://files.tmdb.org/p/exports/movie_ids_${date}.json.gz`;
+        //const url = `http://files.tmdb.org/p/exports/movie_ids_${date}.json.gz`;
+        const url = `http://files.tmdb.org/p/exports/movie_ids_10_25_2020.json.gz`;
         const path = Path.resolve(__dirname, 'download', name);
         const fileStream = fs.createWriteStream(path);
 
@@ -137,10 +147,10 @@ const startMoviesParsing = async (count, delay) => {
         // Decompressed data and make them to string
         const decompressed = await ungzip(data);
         const stringData = await decompressed.toString();
-        console.log("[PARSER]: Start parsing decompressed data..".green);
+        console.log("[PARSER]: Start decompressed data and convert them..".green);
         // Convert string of objects to array of objects
         const arr = stringData.split("\n");
-        console.log("[PARSER]: Result data length: ".green, colors.cyan(arr.length));
+        console.log("[PARSER]: Decompressed data length is: ".green, colors.cyan(arr.length));
         // Create array for most popular content
         const mostPopular = [];
         // Run cycle and filter the prev data array (sort by popularity)
@@ -154,19 +164,72 @@ const startMoviesParsing = async (count, delay) => {
                 }
             }
         });
-        console.log("[PARSER]: New data length (by popularity): ".green, colors.cyan(mostPopular.length));
+        console.log("[PARSER]: Filtered (popularity > 1.00) data array length is: ".green, colors.cyan(mostPopular.length));
         // Save ID's array to database
         const saveAllMoviesID = allMoviesID({items: mostPopular});
         await saveAllMoviesID.save();
+        console.log("[PARSER]: Saved to database array with id's, length is: ".green, colors.cyan(mostPopular.length));
         // Call downloader function for parsing or update
         await contentDownloader(mostPopular, count, delay)
-
+        // Convert JPG posters and backdrops to webp format
+        await convertImages('img_movie_posters/*.{jpg,png}', 'build/images_posters');
+        await convertImages('img_movie_backdrop/*.{jpg,png}', 'build/images_backdrop');
+        // Delete folders with JPG posters and backdrops
+        await deleteFolder("img_movie_posters");
+        await deleteFolder("img_movie_backdrops");
     } catch (e) {
         console.log(colors.red(e.message))
     }
 
 };
 
+const checkAndCompareNewMovies = async (count, delay) => {
+    try {
+        // Get current date
+        const date = await getCurrentDate();
+        // Get backup of current day
+        await getFullDayBackup(date);
+        // Get compressed Day import for the prev day
+        const data = fs.readFileSync(`download/${date}.json.gz`);
+        // Decompressed data and make them to string
+        const decompressed = await ungzip(data);
+        const stringData = await decompressed.toString();
+        console.log("[UPDATER]: Start decompressed data and convert them..".green);
+        // Convert string of objects to array of objects
+        const arr = stringData.split("\n");
+        console.log("[UPDATER]: Decompressed data length is: ".green, colors.cyan(arr.length));
+        // Array with new data
+        const newData = [];
+
+        arr.forEach((item) => {
+            if(item){
+                const obj = JSON.parse(item);
+
+                if(obj.popularity > 1) {
+                    // Save only ID's on array
+                    newData.push(obj.id)
+                }
+            }
+        });
+        console.log("[UPDATER]: Filtered (popularity > 1.00) data array length is:".green, colors.cyan(newData.length));
+        // Array with old data from database (only ID's)
+        const oldData = await allMoviesID.find({});
+        console.log("[UPDATER]: Previous period data array (from database) length is:".green, colors.cyan(oldData[0].items.length));
+        // Array with difference between New Data and Old data from database
+        let difference = newData.filter(x => !oldData[0].items.includes(x));
+        console.log("[UPDATER]: Difference between new data and array with id's (from database) is:".green, colors.cyan(difference.length));
+        // Call downloader function for parsing or update
+        await contentDownloader(difference, count, delay);
+        // Convert JPG posters and backdrops to webp format
+        await convertImages('img_movie_posters/*.{jpg,png}', 'build/images_posters');
+        await convertImages('img_movie_backdrop/*.{jpg,png}', 'build/images_backdrop');
+        // Delete folders with JPG posters and backdrops
+        await deleteFolder("img_movie_posters");
+        await deleteFolder("img_movie_backdrops");
+    } catch (e) {
+        console.log(colors.red(e.message))
+    }
+};
 const contentDownloader = async (array, count, delay) => {
     try {
         // Timer function for get estimated time
@@ -181,7 +244,7 @@ const contentDownloader = async (array, count, delay) => {
         };
 
         let success = 0;
-        let partial = 0;
+        let missed = 0;
 
         console.log("///////////////////////////////////".yellow);
         console.log("//// START DOWNLOAD ONE BY ONE ////".yellow);
@@ -204,7 +267,7 @@ const contentDownloader = async (array, count, delay) => {
                 }
                 // Step by step getting data from two sides TMDB and VideoCDN
                 for (const id of roundToDownload) {
-                    console.log("[DOWNLOADER]: Get details by id:".green, colors.cyan(id));
+                    console.log("[DOWNLOADER]:  Get details from TMDB by id:".green, colors.cyan(id));
                     // Getting from TMDB
                     const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&${language}&${region}`);
                     const data = await response.json();
@@ -212,7 +275,7 @@ const contentDownloader = async (array, count, delay) => {
                     // Getting from VideoCDN
                     const videoCdnResponse = await fetch(`https://videocdn.tv/api/movies?api_token=QDH5tZqrotr27szq3U9Yx2lEgunhKbuo&direction=desc&field=global&limit=10&ordering=last_media_accepted&imdb_id=${data.imdb_id}`);
                     const videoCdnData = await videoCdnResponse.json();
-                    console.log("[DOWNLOADER]: VideoCDN by id".green, colors.cyan(id), "exist's:".green, colors.cyan(videoCdnData.data.length ? "true" : "false"));
+                    console.log("[DOWNLOADER]: VideoCDN by id".green, colors.cyan(id), "exist's:".green, colors.cyan(videoCdnData.data.length ? "true" : "false"), videoCdnData.data.length ? `title: "${colors.cyan(data.title)}"`.green : "");
 
                     // If VideoCDN response contains needed concat data with previous object
                     if (videoCdnData.data.length && data.poster_path && data.backdrop_path) {
@@ -223,8 +286,8 @@ const contentDownloader = async (array, count, delay) => {
                         //Create links for download posters and backdrops
                         const poster_link = `https://image.tmdb.org/t/p/w342${data.poster_path}`;
                         const backdrop_link = `https://image.tmdb.org/t/p/w1280${data.backdrop_path}`;
-                        console.log(`[DOWNLOADER]: Get image poster from:`.green, colors.cyan(poster_link));
-                        console.log(`[DOWNLOADER]: Get image backdrop from:`.green, colors.cyan(backdrop_link));
+                        console.log(`[DOWNLOADER]: Get image poster from:`.green, colors.cyan(data.poster_path));
+                        console.log(`[DOWNLOADER]: Get image backdrop from:`.green, colors.cyan(data.backdrop_path));
 
                         // Where put images
                         const poster = Path.resolve(__dirname, 'img_movie_posters', data.poster_path.slice(1));
@@ -233,14 +296,7 @@ const contentDownloader = async (array, count, delay) => {
                         await downloadFile(poster_link, poster);
                         await downloadFile(backdrop_link, backdrop);
 
-                        // Convert JPG posters and backdrops to webp format
-                        await convertImages(`img_movie_posters${data.poster_path}`, `img_movie_posters${data.poster_path.slice(0, -4)}.webp`);
-                        await convertImages(`img_movie_backdrops${data.backdrop_path}`, `img_movie_backdrops${data.backdrop_path.slice(0, -4)}.webp`);
-
-                        // Delete JPG images
-                        await deleteItem(`img_movie_posters${data.poster_path}`);
-                        await deleteItem(`img_movie_backdrops${data.backdrop_path}`);
-
+                        // Add new link to movie data object
                         data.poster_path = `${data.poster_path.slice(0, -4)}.webp`;
                         data.backdrop_path = `${data.backdrop_path.slice(0, -4)}.webp`;
 
@@ -250,64 +306,20 @@ const contentDownloader = async (array, count, delay) => {
                         success = success + 1;
                     } else {
                         // If doesnt contain needed data push to partial object on database for next time check
-                        const PARTIAL_DOWNLOADED = new partialMovie(data);
-                        await PARTIAL_DOWNLOADED.save();
-                        partial = partial + 1;
+                        // const PARTIAL_DOWNLOADED = new partialMovie(data);
+                        // await PARTIAL_DOWNLOADED.save();
+                        missed = missed + 1;
                     }
 
                 }
                 // Some useful system information
                 console.log("..of array length:".green, colors.cyan(array.length));
                 console.log("Success:".green, colors.cyan(success));
-                console.log("Partial:".green, colors.cyan(partial));
+                console.log("Missed:".green, colors.cyan(missed));
                 await timer(array.length, count);
                 console.log("------------------------------".yellow);
             }
         }, delay);
-    } catch (e) {
-        console.log(colors.red(e.message))
-    }
-};
-
-
-const checkAndCompareNewMovies = async (count, delay) => {
-    try {
-        // Get current date
-        const date = await getCurrentDate();
-        // Get backup of current day
-        await getFullDayBackup(date);
-        // Get compressed Day import for the prev day
-        const data = fs.readFileSync(`download/${date}.json.gz`);
-        // Decompressed data and make them to string
-        const decompressed = await ungzip(data);
-        const stringData = await decompressed.toString();
-        console.log("[UPDATER]: Start parsing decompressed data..".green);
-        // Convert string of objects to array of objects
-        const arr = stringData.split("\n");
-        console.log("[UPDATER]: Result data length: ".green, colors.cyan(arr.length));
-        // Array with new data
-        const newData = [];
-
-        arr.forEach((item) => {
-            if(item){
-                const obj = JSON.parse(item);
-
-                if(obj.popularity > 1) {
-                    // Save only ID's on array
-                    newData.push(obj.id)
-                }
-            }
-
-        });
-        console.log("[UPDATER]: New data length is:".green, colors.cyan(newData.length));
-        // Array with old data from database (only ID's)
-        const oldData = await allMoviesID.find({});
-        console.log("[UPDATER]: Old data length is:".green, colors.cyan(oldData[0].items.length));
-        // Array with difference between New Data and Old data from database
-        let difference = newData.filter(x => !oldData[0].items.includes(x));
-        console.log("[UPDATER]: Difference length is:".green, colors.cyan(difference.length));
-        // Call downloader function for parsing or update
-        await contentDownloader(difference, count, delay);
     } catch (e) {
         console.log(colors.red(e.message))
     }
@@ -323,12 +335,12 @@ const initialization = async () => {
 
         if (movies.length) {
             // If movies exists -> start check updates and compare with prev day movies array
-            console.log("[INITIALIZATION]: OK! Movies in database exists".yellow);
-            console.log("[INITIALIZATION]: Next step is try to check new movies..".yellow);
+            console.log("[INITIALIZATION]: OK! Array with Movies id's in database exists".yellow);
+            console.log("[INITIALIZATION]: Next step is try to check new movies data for this day and compare them with previous data array from database..".yellow);
             await checkAndCompareNewMovies(countOfParsingItems, delay);
         } else {
-            console.log("[INITIALIZATION]: WARNING! No movies in database".yellow);
-            console.log("[INITIALIZATION]: Next step is try parsing movies..".yellow);
+            console.log("[INITIALIZATION]: WARNING! Array with Movies id's in database doesn't exists!".yellow);
+            console.log("[INITIALIZATION]: Next step is try download movies data, parsing them, and create array with movies id's in database ..".yellow);
             await startMoviesParsing(countOfParsingItems, delay);
         }
 
@@ -390,19 +402,4 @@ const startServerMenu = async () => {
 
 startServerMenu();
 
-// if (readlineSync.keyInYN('Do you want this module?')) {
-//     // 'Y' key was pressed.
-//     console.log('Installing now...');
-//     // Do something...
-// } else {
-//     // Another key was pressed.
-//     console.log('Searching another...');
-//     // Do something...
-// }
-
-
-
-
 app.get('/', (req, res) => res.send('Welcome! Node server on air!'));
-
-
